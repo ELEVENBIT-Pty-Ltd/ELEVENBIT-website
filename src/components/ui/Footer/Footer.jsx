77b2914872912c3dd75c0144c4c15a41f86fe5e1
Xoli <xolinxiweni@gmail.com>
2025-05-19 22:05:36 +0200

    first commit

--- src/components/ui/Footer/Footer.jsx
+++ src/components/ui/Footer/Footer.jsx
@@ -50,3 +50,9 @@ const Footer = () => {
 export default Footer
 
 
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ELEVENBIT-Pty-Ltd/ELEVENBIT-website.git
+git push -u origin main
